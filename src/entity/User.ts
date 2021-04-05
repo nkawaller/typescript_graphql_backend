b0fbabf9480b9fbf49719fbbd2d1ca9fe8eebe70
Nathan Kawaller <kawaller@gmail.com>
2021-04-05 09:27:09 -0400

    register resolver

---
+++ src/entity/User.ts
@@ -0,0 +1,30 @@
+import { Field, ObjectType, ID } from "type-graphql";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+
+    @Field(() => ID)
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Field()
+    @Column()
+    firstName: string;
+
+    @Field()
+    @Column()
+    lastName: string;
+
+    @Field()
+    @Column("text", { unique: true})
+    email: string;
+
+    @Field()
+    name: string;
+
+    @Column()
+    password: string;
+
+}
