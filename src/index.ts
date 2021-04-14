3ea4e1fb04ece4d559349195ce9f9675a615c416
Nathan Kawaller <kawaller@gmail.com>
2021-04-14 09:53:45 -0400

    created ConfirmUser mutation

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import { RegisterResolver } from "./modules/user/Register";
 import { redis } from "./redis"
 import { LoginResolver } from "./modules/user/Login";
 import { MeResolver } from "./modules/user/Me";
+import { ConfirmUserResolver } from "./modules/user/ConfirmUser";
 
 
 dotenv.config();
@@ -27,7 +28,7 @@ const main = async () => {
   await createConnection()
 
     const schema = await buildSchema({
-        resolvers: [RegisterResolver, LoginResolver, MeResolver],
+        resolvers: [RegisterResolver, LoginResolver, MeResolver, ConfirmUserResolver],
         authChecker: ({ context: {req} }) => {
           return !!req.session.userId
         }
