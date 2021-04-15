196ad724cba0de522eee0dcfdd9d9e6538f6b5cb
Nathan Kawaller <kawaller@gmail.com>
2021-04-15 09:57:20 -0400

    password validation

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,9 +1,10 @@
 import { Length, IsEmail } from "class-validator";
+import { PasswordInput } from "../../shared/PasswordInput";
 import { Field, InputType } from "type-graphql";
 import { doesEmailAlreadyExist } from "./doesEmailAlreadyExist";
 
 @InputType()
-export class RegisterInput {
+export class RegisterInput extends PasswordInput{
   @Field()
   @Length(1, 255)
   firstName: string;
@@ -17,6 +18,4 @@ export class RegisterInput {
   @doesEmailAlreadyExist({message: "that email is already used"})
   email: string;
 
-  @Field()
-  password: string;
 }
