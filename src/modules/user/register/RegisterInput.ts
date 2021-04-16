162ab2dcdd491931fd117ba6a004d5e6a8a82837
Nathan Kawaller <kawaller@gmail.com>
2021-04-15 20:42:25 -0400

    removed test mixin

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -2,10 +2,9 @@ import { Length, IsEmail } from "class-validator";
 import { PasswordMixin } from "../../shared/PasswordInput";
 import { Field, InputType } from "type-graphql";
 import { doesEmailAlreadyExist } from "./doesEmailAlreadyExist";
-import { OkMixin } from "../../shared/OkMixin"
 
 @InputType()
-export class RegisterInput extends OkMixin(PasswordMixin(class{})) {
+export class RegisterInput extends PasswordMixin(class{}) {
   @Field()
   @Length(1, 255)
   firstName: string;
